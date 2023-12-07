10c906d4879efa8083a7c372a8452809b63030b6
Boris <78667809+ALCON2025@users.noreply.github.com>
2023-12-07 14:21:54 -0500

    Add files via upload

---
+++ QUITZ JAVASCRIPT/script.js
@@ -0,0 +1,409 @@
+const questions=[
+    {
+        question:"Qué son los páramos?",
+        answers: [
+            {text: "Son ecosistemas que se encuentran en los bosques húmedos de la Costa", correct: false},
+            {text: "Son ecosistemas de montañas de las partes altas de los Andes. En 2900 m hasta 5000 m s.n.m.", correct: true},
+          
+        ]
+    },
+    {
+        question:"Cuáles son los ejemplos de especies vegetales de los páramos?",
+        answers: [
+            {text: "fraijelones,achupallas, arbustos xerofiticos, penachos y almohadillas", correct: true},
+            {text: "cactus, pasto, llerva verde", correct: false},
+            
+         
+        ]
+
+    },
+    {
+        question:"Indique la fauna de los páramos?",
+        answers: [
+            {text: "venado, llama, borregos", correct: false},
+            {text: "ranas,lagartijas,cóndor, el curiquingue, el quinde", correct: true},
+         
+        ]
+
+    },
+    {
+        question:"Dónde se ubican los bosques montanos o andinos?",
+        answers: [
+            {text: "Se ubican entre los 1200 y 1500 m s.n.m", correct: true},
+            {text: "Se ubican en el perfil costanero", correct: false},
+           
+        ]
+
+    },
+    {
+        question:"Cuál es la flora de los bosques montanos o andinos?",
+        answers: [
+            {text: "ciruelas, uvas, frutillas, ovos, drocellas", correct: false},
+            {text: "podocarpus,robles, cedros, musgos, hapáticas, orquídeas y bromelias", correct: true},
+         
+        ]
+
+    },
+    {
+        question:"Cuál es la fauna de los bosques montanos?",
+        answers: [
+            {text: "Gallito de la peña, gavilanes y colibríes, guantas, guatusas, osos de anteojo y pumas", correct: true},
+            {text: "leopardo, serpientes, cocodrilos, avestruz", correct: false},
+           
+         
+        ]
+
+    },
+    {
+        question:"Dónde se encuentran los valles secos?",
+        answers: [
+            {text: "Se encuentran en el lado seco de Guayaquil, el Oro y los Ríos", correct: false},
+            {text: "Se encuentran en medio de la cordillera occidental y oriental:el Chota, Guayllabamba y Catamayo", correct: true},
+         
+        ]
+
+    },
+    {
+        question:"Cuál es la flora y la fauna de los valles secos?",
+        answers: [
+            {text: "tortugas, venados, tigrillos, monos, eucalipto, manzanilla", correct: false},
+            {text: "cactus, acasias, pencos, algarrobos, ratones de campo, quilicos y tayos, armadillos y lobos", correct: true},
+           
+           
+         
+        ]
+
+    },
+    {
+        question:"Qué es el bosque húmedo tropical y qué incluye?",
+        answers: [
+            {text: "Es una selva llena de gavilanes y terreno seco , sus tierras componen rocas y montañas", correct: false},
+            {text: "Es el lugar más original y biodiverso. Incluyen selvas amazónicas y esmeraldeñas y se ubican desde los 0 metros de nviel del mar hasta 600 m s.n.m.", correct: true},
+         
+        ]
+
+    },
+    {
+        question:"Cuál es la flora y fauna del bosque húmedo tropical?",
+        answers: [
+            {text: "monos, pecaríes, tapires, manatíes, delfines rosados, jaguares, trigillos, reptiles, anacondas y caimanes, llanas, bejucos, arbustos y plantas herbáceas", correct: true},
+            {text: "colibríes, gavilanes, cóndor, plantas y flores  como la orquídea", correct: false},
+            
+         
+        ]
+
+    },
+    {
+        question:"Dónde se ubica el bosque seco de la Costa?",
+        answers: [
+            {text: "Se ubica entre Esmeraldas y el Carchi. Su rango de altitud esta ente 0 y 540 m s.n.m", correct: false},
+            {text: "Se ubica en Guayas y Manabí, principalmente. Su rango de altitud esta entre 0 y 840 m s.n.m.", correct: true},
+         
+        ]
+
+    },
+    {
+        question:"Cuál es la flora y fauna del bosque seco de la Costa?",
+        answers: [
+            {text: "porotillo, ceibo,cactos, arbustos, muyugos,lechuzas, guatusas, armadillos, osos hormigueros, murciélagos y la raposa o zarigueya", correct: true},
+            {text: "cacao, cafe, platano, elfante, hipopótamo", correct: false},
+           
+         
+        ]
+
+    },
+    {
+        question:"Qué son los humedales?",
+        answers: [
+            {text: "Se define por la presencia de aire húmeod durante períodos largos de tiempo", correct: false},
+            {text: "Se definen por la presencia de agua durante períodos prolongados de tiempo.", correct: true},
+         
+        ]
+
+    },
+    {
+        question:"Por qué son importantes los humedales?",
+        answers: [
+            {text: "Son importantes porque garantizan la protección y la restauración del recurso hídrico", correct: true},
+            {text: "Son importantes porque garantizan la protección de la humedad en la selva", correct: false},
+           
+        ]
+
+    },
+    {
+        question:"Cuál es la flora y fauna de los humedales?",
+        answers: [
+            {text: "desierto, borregos, caballos, vacas", correct: false},
+            {text: "manglares, crustáceos, aves, garzas y cormoranes.", correct: true},
+         
+        ]
+
+    },
+    {
+        question:"Dónde se ubican las Islas Galápagos?",
+        answers: [
+            {text: "Se ubican a 1300 km de la Costa de Colombia", correct: false},
+            {text: "Se ubican a 1000 KM de la Costa ecuatoriana", correct: true},
+         
+        ]
+
+    },
+    {
+        question:"Dónde se encuentran ubicada la Reserva Biológica de Limoncocha?",
+        answers: [
+            {text: "Se encuentra en la provincia de Sucumbios, al norte de la Amazonía", correct: true},
+            {text: "Se euncuetra en la provincia del Napo al norte de la Amazonía", correct: false},
+
+         
+        ]
+
+    },
+    {
+        question:"Qué acciones realizan los guardabosques?",
+        answers: [
+            {text: "Cuidan y evitan la contaminación de la minería ilegal", correct: false},
+            {text: "Cuidan y evitan el tráfico de la vida silvestre", correct: true},
+         
+        ]
+
+    },
+    {
+        question:"Dónde se encuentran ubicada la Reserva ecológica Cotacachi-Cayapas?",
+        answers: [
+            {text: "Se encuentra en las provincias de Esmeraldas e Imbabura", correct: true},
+            {text: "Se encuentra en las provincias de Pichincha y Manabí", correct: false},
+            
+        ]
+
+    },
+    {
+        question:"Qué programas se realizan en esta reserva ecológica?",
+        answers: [
+            {text: "Coloación de represas de agua y molinos de viento", correct: false},
+            {text: "Seguimiento, monitoreo y evaluación de turistas. Capacitación a miembros  de Piñan y Guananí en gestión turística. Investigación y monitoreo", correct: true},
+         
+        ]
+
+    },
+    {
+        question:"Qué significa adaptarse?",
+        answers: [
+            {text: "Signinfica no cambiar y permanecer estable y constante para salvaguardar su vida en el medio ambiente.", correct: false},
+            {text: "Significa modificar su organismo para mejorar su relación con el medio y poder alimentarse, defenderse y reproducirse.", correct: true},
+         
+        ]
+
+    },
+    {
+        question:"Qué es la adaptación?",
+        answers: [
+            {text: "Es una característica que puede ser física o de conducta.", correct: true},
+            {text: "Es una manera de pensar positivamente", correct: false},
+            
+         
+        ]
+
+    },
+    {
+        question:"Indique algunas adaptaciones de los animales al tipo de alimentación?",
+        answers: [
+            {text: "Poseen dientes pequeños. garras rectas.", correct: false},
+            {text: "Tienen dientes filosos y grandes. pico curvo y fuerte. garras afilads . piezas molares desarrolladas y planas. Sistema digestivo largo.", correct: true},
+         
+        ]
+
+    },
+    {
+        question:"Qué son las adaptaciones morfológicas?",
+        answers: [
+            {text: "Permiten nadar libremente y volar.", correct: false},
+            {text: "Permiten extraer, capturar o masticar su alimento.", correct: true},
+         
+        ]
+
+    },
+    {
+        question:"Indique algunas adaptaciones reproductivas?",
+        answers: [
+            {text: "Reproducción única sexual.", correct: false},
+            {text: "Reproduccion sexual y asexual.", correct: true},
+         
+        ]
+
+    },
+    {
+        question:"Qué es el hermafroditismo?",
+        answers: [
+            {text: "Es una adaptación que le permite a un ser vivo tener sistemas reproductores femeninos y masculinos en el mismo organismo.", correct: true},
+            {text: "Es una adaptación que tiene un solo organo reproductor.", correct: false},
+            
+         
+        ]
+
+    },
+    {
+        question:"Indique algunas adaptaciones de desplazamiento?",
+        answers: [
+            {text: "No han cambiado mucho los animales en sus extremidades.", correct: false},
+            {text: "Alas y patas de formas y tamaños diferentes. Gusanos con poderosos músculos.Extremidades para saltar y nadar. Aves con cuerpos aerodinámicos. Humanos con el dedo pulgar que ayuda mucho en tomar cosas.", correct: true},
+         
+        ]
+
+    },
+    {
+        question:"Qué son las adaptaciones de protección?",
+        answers: [
+            {text: "Es una carcterística que los confunde en el medio ambiente.", correct: false},
+            {text: "Es una característica que permite a los animales sobrevivir a sus depredadores y a la dureza del clima.", correct: true},
+         
+        ]
+
+    },
+    {
+        question:"Qué es el mimetismo?",
+        answers: [
+            {text: "Un organimso se confunde con su medio para pasar desapercibido.", correct: false},
+            {text: "Propiedad que poseen algunos animales y plantas de asemejarse a otros seres de su entorno.", correct: true},
+         
+        ]
+
+    },
+    {
+        question:"Qué es el camuflaje?",
+        answers: [
+            {text: "Un organismo se confunde con su medio para pasar desapercibido", correct: true},
+            {text: "Los animales presentan estructuras como dientes o espinas que producen sustancias tóxicas para sus enemigos.", correct: false},
+            
+         
+        ]
+
+    },
+    {
+        question:"Para qué les sirve el veneno a algunos animales?",
+        answers: [
+            {text: "Para dormir un sueño más profundo.", correct: false},
+            {text: "Los animales presentan estructuras como dientes o espinas que producen sustancias tóxicas para sus enemigos.", correct: true},
+         
+        ]
+
+    },
+    {
+        question:"Qué función cumple el caparazón de algunos animales?",
+        answers: [
+            {text: "Propiedad que poseen algunos animales para protegerse del frío.", correct: false},
+            {text: "Proporcinoan protección frente a golpes, al ataque de depredadores y al mal tiempo. ", correct: true},
+         
+        ]
+
+    },
+    {
+        question:"Cuáles son algunas adaptaciones al frío?",
+        answers: [
+            {text: "Generar menos flujo sanguíneo a la piel, desarrollar pelaje como el soso y acumular grasa para tener calor.", correct: true},
+            {text: "Generar uñas más largas para alimentarse mejor.", correct: false},
+           
+         
+        ]
+
+    },
+    {
+        question:"Qué son los organismos homeotermos?Ejemplos.",
+        answers: [
+            {text: "Mantienen la temperatura interna al medio por ejemplo las lagartijas tienene temperatura interna igual que la externa", correct: false},
+            {text: "Adaptan la temperatura interna para ello por ejemplo los humanos sudan y los perros jadean", correct: true},
+         
+        ]
+
+    },
+    {
+        question:"Qué son organismos poiquilotermos?Ejemplo ",
+        answers: [
+            {text: "Mantienen su temperatura interna", correct: false},
+            {text: "Adaptan su temperatura interna al medio por ejempl las lagartijas tienen su temperatura interna igual que la exsterna.", correct: true},
+         
+        ]
+
+    }
+];
+
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+function showQuestion(){
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex +1 ;
+    questionElement.innerHTML = questionNo + "." + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+    });
+}
+
+function resetState(){
+    nextButton.style.display = "none";
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+        score++;
+    }else{
+        selectedBtn.classList.add("incorrect");
+    }
+    Array.from(answerButtons.children).forEach(button => {
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
+    }
+
+    function showScore() {
+
+        resetState();
+        questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+        nextButton.innerHTML = "Play Again"
+        nextButton.style.display = "block";
+    }
+
+    function handleNextButton(){
+        currentQuestionIndex++;
+        if(currentQuestionIndex < questions.length){
+            showQuestion();
+        }else{
+            showScore();
+        }
+    }
+    nextButton.addEventListener("click", ()=>{
+        if(currentQuestionIndex < questions.length){
+            handleNextButton();
+        }else{
+            startQuiz();
+        }
+    });
+
+startQuiz();
